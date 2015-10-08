96b8b6eb88f229295109ec120c5192ab41beadd8
Aaron Hardy <aahardy@adobe.com>
2015-10-08 10:34:00 -0600

    Adding copying of build templates.

--- index.js
+++ index.js
@@ -19,9 +19,12 @@ var ALL_CAPABILITY_NAMES = [
   'resources'
 ].concat(DELEGATE_CAPABILITY_NAMES);
 
-var CONTAINER_TEMPLATE_NAME = 'container.txt';
-var CONTAINER_OUTPUT_NAME = 'container.js';
-var EXTENSION_DESCRIPTOR_NAME = 'extension.json';
+var CONTAINER_TEMPLATE_FILENAME = 'container.txt';
+var CONTAINER_OUTPUT_FILENAME = 'container.js';
+var HTML_TEMPLATE_FILENAME = 'index.html';
+var EXTENSION_DESCRIPTOR_FILENAME = 'extension.json';
+var OUTPUT_DIRNAME = 'dist';
+var TEMPLATES_DIRNAME = 'buildTemplates';
 
 function wrapInFunction(content, argNames) {
   var argsStr = argNames ? argNames.join(', ') : '';
@@ -69,13 +72,45 @@ var augmentResources = function(capabilities, extensionDescriptor, extensionPath
 };
 
 module.exports = function(gulp) {
-  gulp.task('buildContainer', function() {
+  gulp.task('copyTemplates', function() {
+    var templatesToCopy = [];
+
+    // Only copy these files into the project's templates directory if they don't already exist
+    // there. This allows the engineer to make edits to index.html and container.txt for testing
+    // purposes.
+
+    try {
+      fs.lstatSync(path.join(TEMPLATES_DIRNAME, HTML_TEMPLATE_FILENAME));
+    } catch (e) {
+      templatesToCopy.push(path.join(__dirname, TEMPLATES_DIRNAME, HTML_TEMPLATE_FILENAME));
+    }
+
+    try {
+      fs.lstatSync(path.join(TEMPLATES_DIRNAME, CONTAINER_TEMPLATE_FILENAME));
+    } catch (e) {
+      templatesToCopy.push(path.join(__dirname, TEMPLATES_DIRNAME, CONTAINER_TEMPLATE_FILENAME));
+    }
+
+    if (templatesToCopy.length) {
+      return gulp
+        .src(templatesToCopy)
+        .pipe(gulp.dest(TEMPLATES_DIRNAME));
+    }
+  });
+
+  gulp.task('copyHTMLToOutput', ['copyTemplates'], function() {
+    return gulp
+      .src(path.join(TEMPLATES_DIRNAME, HTML_TEMPLATE_FILENAME))
+      .pipe(gulp.dest(OUTPUT_DIRNAME));
+  });
+
+  gulp.task('buildContainer', ['copyTemplates'], function() {
     // When running this task from a turbine extension project we want to include the
     // extension.json from that extension as well as any extensions we find under its
     // node_modules.
     // When running this task from within this builder project we really only care
     // about any extensions we find under this project's node_modules.
-    var extensionDescriptorPaths = glob.sync('{node_modules/*/,}' + EXTENSION_DESCRIPTOR_NAME);
+    var extensionDescriptorPaths = glob.sync('{node_modules/*/,}' + EXTENSION_DESCRIPTOR_FILENAME);
 
     var capabilities = {};
 
@@ -92,14 +127,7 @@ module.exports = function(gulp) {
       augmentResources(capabilities, extensionDescriptor, extensionPath);
     });
 
-    try {
-      fs.lstatSync(CONTAINER_TEMPLATE_NAME);
-    } catch (e) {
-      throw new Error('A container template named ' + CONTAINER_TEMPLATE_NAME +
-      ' must exist in the project.');
-    }
-
-    var container = gulp.src([CONTAINER_TEMPLATE_NAME]);
+    var container = gulp.src(path.join(TEMPLATES_DIRNAME, CONTAINER_TEMPLATE_FILENAME));
 
     ALL_CAPABILITY_NAMES.forEach(function(capabilityName) {
       container = container.pipe(
@@ -108,39 +136,49 @@ module.exports = function(gulp) {
     });
 
     return container
-      .pipe(rename(CONTAINER_OUTPUT_NAME))
-      .pipe(gulp.dest('dist'));
+      .pipe(rename(CONTAINER_OUTPUT_FILENAME))
+      .pipe(gulp.dest(OUTPUT_DIRNAME));
   });
 
-  gulp.task('buildEngine', function() {
-    var turbinePath = path.join('node_modules/turbine');
+  var pkg = require('./package.json');
 
-    try {
-      fs.lstatSync(turbinePath);
-    } catch (e) {
-      throw new Error('Turbine must be installed as a dependency of the project.');
-    }
+  // If we're running this builder from the turbine project then buildEngine is already
+  // added as a task and there is no reason to run it indirectly.
+  if (pkg.name !== 'turbine') {
+    gulp.task('buildEngine', function() {
+      var turbinePath = path.join('node_modules/turbine');
 
-    execSync('gulp buildEngine', {
-      cwd: turbinePath,
-      stdio: [0, 1, 2] // Output the child's process output to the console.
+      execSync('gulp buildEngine', {
+        cwd: turbinePath,
+        stdio: [0, 1, 2] // Output the child's process output to the console.
+      });
+
+      return gulp
+        .src(path.join(turbinePath, path.join(OUTPUT_DIRNAME, 'engine.js')))
+        .pipe(gulp.dest(OUTPUT_DIRNAME));
     });
+  }
 
-    return gulp
-      .src([path.join(turbinePath, 'dist/engine.js')])
-      .pipe(gulp.dest('dist'));
+  gulp.task('watch', ['copyTemplates'], function() {
+    gulp.watch(path.join(TEMPLATES_DIRNAME, CONTAINER_TEMPLATE_FILENAME), ['buildContainer']);
+    gulp.watch(path.join(TEMPLATES_DIRNAME, HTML_TEMPLATE_FILENAME), ['copyHTMLToOutput']);
   });
 
-  gulp.task('watch', function() {
-    return gulp.watch(['container.txt'], ['buildContainer']);
-  });
-
-  gulp.task('serve', function() {
-    return gulp.src('./')
+  gulp.task('serve', ['copyHTMLToOutput', 'buildContainer', 'buildEngine'], function() {
+    return gulp.src(OUTPUT_DIRNAME)
       .pipe(webserver({
-        port: 7000
+        port: 7000,
+        livereload: true,
+        open: true
       }));
   });
 
-  gulp.task('default', ['buildEngine', 'buildContainer', 'serve', 'watch']);
+  gulp.task('default', [
+    'copyTemplates',
+    'copyHTMLToOutput',
+    'buildEngine',
+    'buildContainer',
+    'serve',
+    'watch'
+  ]);
 };
