b1dec463b1da9f92ba4a8a5844d0f851f1cd533a
Serban Stancu <stancu@adobe.com>
2015-10-10 08:56:48 -0600

    Build container when  folder changes.

--- tasks/constants.js
+++ tasks/constants.js
@@ -4,5 +4,7 @@ module.exports = {
   CONTAINER_TEMPLATE_FILENAME: 'container.txt',
   HTML_TEMPLATE_FILENAME: 'index.html',
   OUTPUT_DIRNAME: 'dist',
-  TEMPLATES_DIRNAME: 'buildTemplates'
+  TEMPLATES_DIRNAME: 'buildTemplates',
+  SRC_DIRNAME: 'src',
+  SRC_FILENAMES: '/**/*.js'
 };
--- tasks/watch.js
+++ tasks/watch.js
@@ -6,7 +6,10 @@ var constants = require('./constants');
 module.exports = function(gulp) {
   gulp.task('watch', ['copyTemplates'], function() {
     gulp.watch(
-      path.join(constants.TEMPLATES_DIRNAME, constants.CONTAINER_TEMPLATE_FILENAME),
+      [
+        path.join(constants.TEMPLATES_DIRNAME, constants.CONTAINER_TEMPLATE_FILENAME),
+        path.join(constants.SRC_DIRNAME, constants.SRC_FILENAMES)
+      ],
       ['buildContainer']
     );
 
