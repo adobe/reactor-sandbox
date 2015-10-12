9efad746bc2aa557a4c452b520e7eff5037518ba
Aaron Hardy <aahardy@adobe.com>
2015-10-12 15:35:46 -0600

    Fixed builder to use the current working directory's turbine code when running the builder from the turbine project.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 'use strict';
 
 module.exports = function(gulp) {
-  require('turbine')(gulp);
+  var turbine;
+  var projectPackage = require(path.join(process.cwd(), 'package.json'));
+
+  // If we're running gulp from the turbine repo, use its code to build the engine. If not, use
+  // the builder's turbine dependency instead.
+  if (projectPackage.name === 'turbine') {
+    turbine = require(path.join(process.cwd(), 'index.js'));
+  } else {
+    turbine = require('turbine');
+  }
+
+  turbine(gulp);
+
   require('./tasks/buildContainer')(gulp);
   require('./tasks/copyEngine')(gulp);
   require('./tasks/copyHTMLToOutput')(gulp);
