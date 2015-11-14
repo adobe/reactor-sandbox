1abbb28524871997559e527e28a769d2b6445787
Serban Stancu <stancu@adobe.com>
2015-11-13 20:28:49 -0700

    Use windgoogles for setting frameboyant and iframe resizer on the iframe view.

--- sandboxIncludes/js/viewSandbox.js
+++ sandboxIncludes/js/viewSandbox.js
@@ -17,9 +17,7 @@
     var getConfigButton = document.getElementById('getConfigButton');
     var setConfigField = document.getElementById('setConfigField');
     var setConfigButton = document.getElementById('setConfigButton');
-
-    var viewIframe = document.getElementById('extensionViewIframe');
-    var windGogglesIframe = require('turbine-windgoggles')(viewIframe);
+    var viewIframeContainer = document.getElementById('iframeContainer');
 
     // Populate View Selector.
     if (extensionDescriptor) {
@@ -43,11 +41,24 @@
       });
     }
 
+    var windGogglesIframe;
     var loadSelectedViewIntoIframe = function() {
+      if (windGogglesIframe) {
+        windGogglesIframe.destroy();
+      }
+
+      var viewIframe = document.createElement('iframe');
+      viewIframe.dataset.frameboyant = true;
+      viewIframe.onload = function() {
+        windGogglesIframe = require('turbine-windgoggles')(viewIframe);
+      };
+
       if (viewSelector.selectedIndex !== -1) {
         var viewPath = viewSelector.options[viewSelector.selectedIndex].value;
         viewIframe.src = 'extensionViews/' + viewPath;
       }
+
+      viewIframeContainer.appendChild(viewIframe);
     };
 
     loadSelectedViewIntoIframe();
