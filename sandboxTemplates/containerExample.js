d9bb1aa3479ba66d2276fd176b683495beba773c
Aaron Hardy <aahardy@adobe.com>
2016-02-12 16:10:35 -0700

    Updated to match latest container format.

--- sandboxTemplates/containerExample.js
+++ sandboxTemplates/containerExample.js
@@ -1,59 +1,71 @@
 'use strict';
 
 module.exports = {
-  propertyConfig: {},
   rules: [
     {
       name: 'Example Rule',
       events: [
         {
-          type: 'def',
-          config: {}
+          delegateId: 'exampleExtension/events/click',
+          settings: {}
         }
       ],
-      conditions:[
+      conditions: [
         {
-          type: 'ghi',
-          config: {}
+          delegateId: 'exampleExtension/conditions/operatingSystem',
+          settings: {}
         }
       ],
       actions: [
         {
-          type: 'jkl',
-          config: {}
+          delegateId: 'exampleExtension/actions/sendBeacon',
+          settings: {}
         }
       ]
     }
   ],
   dataElements: {
     myDataElement: {
-      type: 'mno',
-      config: {}
+      delegateId: 'exampleExtension/dataElements/javascriptVariable',
+      settings: {}
     }
   },
-  appVersion: '52A',
-  buildDate: '2015-03-16 20:55:42 UTC',
-  publishDate: '2015-03-16 14:43:44 -0600',
   extensions: {
-    exampleExtension: {
-      configs: {
-        abc: {}
+    EXa: {
+      name: 'exampleExtension',
+      displayName: 'Example Extension',
+      configurations: {
+        ECa: {
+          settings: {}
+        }
       },
-      events: {
-        def: function(module, require) {}
-      },
-      conditions: {
-        ghi: function(module, require) {}
-      },
-      actions: {
-        jkl: function(module, require) {}
-      },
-      dataElements: {
-        mno: function(module, require) {}
+      delegates: {
+        'exampleExtension/events/click': {
+          displayName: 'Click',
+          script: function(module, require) {}
+        },
+        'exampleExtension/conditions/operatingSystem': {
+          displayName: 'Operating System',
+          script: function(module, require) {}
+        },
+        'exampleExtension/actions/sendBeacon': {
+          displayName: 'Send Beacon',
+          script: function(module, require) {}
+        },
+        'exampleExtension/dataElements/javascriptVariable': {
+          displayName: 'JavaScript Variable',
+          script: function(module, require) {}
+        }
       },
       resources: {
-        myExampleResource: function(module, require) {}
+        'exampleExtension/resources/myExampleResource': {
+          script: function(module, require) {}
+        }
       }
     }
-  }
+  },
+  propertySettings: {},
+  appVersion: '52A',
+  buildDate: '2015-03-16 20:55:42 UTC',
+  publishDate: '2015-03-16 14:43:44 -0600'
 };
