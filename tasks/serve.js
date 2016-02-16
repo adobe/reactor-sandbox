c220b7b94afca0a31e93a00bbd260bbd5793b2bd
Aaron Hardy <aahardy@adobe.com>
2016-02-16 10:11:59 -0700

    Renamed outputSandboxHTMLs.js to outputSandboxHtml.js

--- tasks/serve.js
+++ tasks/serve.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 module.exports = function(gulp) {
   gulp.task('sandbox:serve',
-    ['sandbox:outputSandboxHTMLs', 'sandbox:outputContainer', 'sandbox:outputEngine'],
+    ['sandbox:outputSandboxHtml', 'sandbox:outputContainer', 'sandbox:outputEngine'],
     function() {
       return gulp.src(path.resolve(files.OUTPUT_DIRNAME))
         .pipe(webserver({
