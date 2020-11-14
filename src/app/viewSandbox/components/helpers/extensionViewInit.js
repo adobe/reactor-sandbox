843fed7e5e817a9ee0ffa0168f7604fbad48674d
Serban Stancu <stancu@adobe.com>
2020-11-14 10:09:26 -0700

    Use the error code exposed by PenPal.

--- src/app/viewSandbox/components/helpers/extensionViewInit.js
+++ src/app/viewSandbox/components/helpers/extensionViewInit.js
@@ -10,6 +10,8 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ERR_CONNECTION_DESTROYED } from 'penpal';
 import reportFatalError from './reportFatalError';
 import VIEW_GROUPS from '../../helpers/viewsGroups';
 
@@ -41,7 +43,7 @@ export default ({ extensionBridge, selectedDescriptor, extensionDescriptor, cont
           })
           .catch(reportFatalError);
       } catch (error) {
-        if (error.code !== 'ConnectionDestroyed') {
+        if (error.code !== ERR_CONNECTION_DESTROYED) {
           reportFatalError(error);
         }
       }
