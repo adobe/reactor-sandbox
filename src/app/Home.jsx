83a889c65a8fd8531f547e4518b7154fea87a577
Serban Stancu <stancu@adobe.com>
2020-11-14 23:21:52 -0700

    Remove underline decoration from buttons.

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Button, Text } from '@adobe/react-spectrum';
 import LibrarySandboxIcon from '@spectrum-icons/workflow/FileJson';
 import ViewSandboxIcon from '@spectrum-icons/workflow/AdDisplay';
@@ -10,29 +10,42 @@ import packageJson from '../../package.json';
 
 import './Home.css';
 
-export default () => (
-  <div>
-    <div className="intro">
-      <h1>Reactor Extension Sandbox v{packageJson.version}</h1>
-      <p>
-        Launch, by Adobe, is a next-generation tag management solution enabling simplified
-        deployment of marketing technologies.This project provides a sandbox in which you can
-        manually test your views and your library logic.
-      </p>
+export default () => {
+  const history = useHistory();
 
-      <Link to={NAMED_ROUTES.VIEW_SANDBOX}>
-        <Button variant="primary" marginEnd="size-200" marginTop="size-200">
+  return (
+    <div>
+      <div className="intro">
+        <h1>Reactor Extension Sandbox v{packageJson.version}</h1>
+        <p>
+          Launch, by Adobe, is a next-generation tag management solution enabling simplified
+          deployment of marketing technologies.This project provides a sandbox in which you can
+          manually test your views and your library logic.
+        </p>
+
+        <Button
+          variant="primary"
+          marginEnd="size-200"
+          marginTop="size-200"
+          onPress={() => {
+            history.push(NAMED_ROUTES.VIEW_SANDBOX);
+          }}
+        >
           <ViewSandboxIcon size="L" />
           <Text>Go to View Sandbox</Text>
         </Button>
-      </Link>
 
-      <Link to={NAMED_ROUTES.LIB_SANDBOX}>
-        <Button variant="primary" marginTop="size-200">
+        <Button
+          variant="primary"
+          marginTop="size-200"
+          onPress={() => {
+            history.push(NAMED_ROUTES.LIB_SANDBOX);
+          }}
+        >
           <LibrarySandboxIcon size="L" />
           <Text>Go to Library Sandbox</Text>
         </Button>
-      </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
--- src/app/libSandbox/components/RightColumn.js
+++ src/app/libSandbox/components/RightColumn.js
@@ -1,73 +1,85 @@
 import React from 'react';
 import { Button, Text } from '@adobe/react-spectrum';
 import Draw from '@spectrum-icons/workflow/Draw';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import NAMED_ROUTES from '../../constants';
 
-export default ({ templateLocation, isLatestTemplate }) => (
-  <div className="description-container">
-    <p>Welcome to the Library Sandbox. In this page you can test your library logic.</p>
+export default ({ templateLocation, isLatestTemplate }) => {
+  const history = useHistory();
 
-    {templateLocation === 'sandbox' ? (
-      <>
-        <p>
-          In the left pane of the page you can see a default template that is loaded inside an
-          IFRAME element. The template represents a simple page that loads an empty Launch library.
-        </p>
-        <p>
-          You can customize the template to support your use cases if needed. You can run the
-          command <strong>`npx @adobe/reactor-sandbox init`</strong> inside your extension folder.
-          You can then edit the file created at the following location: `.sandbox/libSandbox.html`.
-          Next time when you refresh the sandbox page iniside the browser, the new content will be
-          shown inside the IFRAME.
-        </p>
-        <p>
-          For more informations about the `init` command you can check the{' '}
-          <a
-            href="https://www.npmjs.com/package/@adobe/reactor-sandbox#configuring-the-sandbox"
-            rel="noreferrer"
-            target="_blank"
+  return (
+    <div className="description-container">
+      <p>Welcome to the Library Sandbox. In this page you can test your library logic.</p>
+
+      {templateLocation === 'sandbox' ? (
+        <>
+          <p>
+            In the left pane of the page you can see a default template that is loaded inside an
+            IFRAME element. The template represents a simple page that loads an empty Launch
+            library.
+          </p>
+          <p>
+            You can customize the template to support your use cases if needed. You can run the
+            command <strong>`npx @adobe/reactor-sandbox init`</strong> inside your extension folder.
+            You can then edit the file created at the following location:
+            `.sandbox/libSandbox.html`. Next time when you refresh the sandbox page iniside the
+            browser, the new content will be shown inside the IFRAME.
+          </p>
+          <p>
+            For more informations about the `init` command you can check the{' '}
+            <a
+              href="https://www.npmjs.com/package/@adobe/reactor-sandbox#configuring-the-sandbox"
+              rel="noreferrer"
+              target="_blank"
+            >
+              documentation
+            </a>{' '}
+            .
+          </p>
+          <p>You can configure extensions, data elements or rules by using the Library Editor.</p>
+
+          <Button
+            variant="cta"
+            onPress={() => {
+              history.push(NAMED_ROUTES.LIB_SANDBOX_RULES_EDITOR);
+            }}
           >
-            documentation
-          </a>{' '}
-          .
-        </p>
-        <p>You can configure extensions, data elements or rules by using the Library Editor.</p>
-        <Link to={NAMED_ROUTES.LIB_SANDBOX_RULES_EDITOR}>
-          <Button variant="cta">
             <Draw />
             <Text>Go to Library Editor</Text>
           </Button>
-        </Link>
-      </>
-    ) : null}
+        </>
+      ) : null}
 
-    {isLatestTemplate === false ? (
-      <p>
-        It seems there is a problem with your template. Follow the steps listed in the left pane of
-        the page in order to contiune.
-      </p>
-    ) : null}
-
-    {templateLocation === 'extension' && isLatestTemplate ? (
-      <>
+      {isLatestTemplate === false ? (
         <p>
-          You can customize the template that is loaded in the left pane of the page by editing the
-          file found at the following location: `.sandbox/libSandbox.html`.
+          It seems there is a problem with your template. Follow the steps listed in the left pane
+          of the page in order to contiune.
         </p>
+      ) : null}
 
-        <p>
-          You can edit the information about saved rules, data elements, and extension configuration
-          by using the Library Editor.
-        </p>
+      {templateLocation === 'extension' && isLatestTemplate ? (
+        <>
+          <p>
+            You can customize the template that is loaded in the left pane of the page by editing
+            the file found at the following location: `.sandbox/libSandbox.html`.
+          </p>
 
-        <Link to={NAMED_ROUTES.LIB_SANDBOX_RULES_EDITOR}>
-          <Button variant="cta">
+          <p>
+            You can edit the information about saved rules, data elements, and extension
+            configuration by using the Library Editor.
+          </p>
+
+          <Button
+            variant="cta"
+            onPress={() => {
+              history.push(NAMED_ROUTES.LIB_SANDBOX_RULES_EDITOR);
+            }}
+          >
             <Draw />
             <Text>Go to Library Editor</Text>
           </Button>
-        </Link>
-      </>
-    ) : null}
-  </div>
-);
+        </>
+      ) : null}
+    </div>
+  );
+};
