3cbbfbc45ed0b2e55bb66e29661fd4468f2fe96a
Brent Hosie <bhosie@adobe.com>
2025-10-02 16:39:07 -0600

    change vite build structure

--- vite.config.js
+++ vite.config.js
@@ -10,18 +10,45 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-// eslint-disable-next-line import/no-extraneous-dependencies
+// eslint-disable-next-line header/header,import/no-extraneous-dependencies
 import { defineConfig } from 'vite';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  // disable vite's default behavior to copy "public" directory assets to the build output
-  publicDir: false,
+  publicDir: 'public',
   build: {
-    outDir: 'dist',
+    outDir: 'build', // output to build/
     emptyOutDir: true,
-    sourcemap: true
+    assetsDir: 'static', // static assets folder
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+      output: {
+        entryFileNames: 'static/js/main.[hash].js',
+        chunkFileNames: 'static/js/[name].[hash].js',
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) {
+            return 'static/css/main.[hash][extname]';
+          }
+          if (name && name.endsWith('.js')) {
+            return 'static/js/main.[hash][extname]';
+          }
+          if (name && name.endsWith('.map')) {
+            if (name.includes('.css')) {
+              return 'static/css/main.[hash][extname]';
+            }
+            if (name.includes('.js')) {
+              return 'static/js/main.[hash][extname]';
+            }
+          }
+          if (name && name.endsWith('.LICENSE.txt')) {
+            return 'static/js/main.[hash][extname]';
+          }
+          return 'static/media/[name].[hash][extname]';
+        }
+      }
+    }
   }
 });
